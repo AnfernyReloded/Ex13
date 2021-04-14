7f3570fa092a7e2d7c7538704d25ae099ce5f8df
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-04-14 08:01:06 -0400

    Exercise13

---
+++ Excercise 13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Excercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //User inputs am integer
+
+        //Console output the number and counts down to zero
+
+        //Console asks if the user wants to enter another number
+        {
+           
+
+            while (true)
+            {
+                Console.Write("Hello user please enter a number: ");
+                int userNumber = int.Parse(Console.ReadLine());
+
+                for (int i = userNumber; i >= 0; --i)
+
+                {
+
+                    Console.WriteLine(i);
+
+
+
+                }
+
+                Console.WriteLine("Would you like to input another number? Y or N");
+                string userInput = Console.ReadLine().ToLower();
+
+                if (userInput == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
